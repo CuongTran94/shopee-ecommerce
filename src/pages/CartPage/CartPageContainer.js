98a1c5942a4f42a10af46beaa74c022fc3a0ee46
apache <nhaphan0073@gmail.com>
2020-08-11 23:56:08 +0700

    fix

--- src/pages/CartPage/CartPageContainer.js
+++ src/pages/CartPage/CartPageContainer.js
@@ -1,19 +1,8 @@
-import React, { useEffect } from "react";
+import React from "react";
 import PropTypes from "prop-types";
 import ListCart from "./ListCart";
-import { useDispatch, useSelector } from "react-redux";
-import { fetchCart } from "../../redux/Cart/cart.actions";
 
-const CartPageContainer = (props) => {
-  const currentUser = useSelector((state) => state.user.currentUser) || {};
-
-  const cart = useSelector((state) => state.cart) || {};
-
-  const dispatch = useDispatch();
-  useEffect(() => {
-    if (currentUser.id === undefined) dispatch(fetchCart(currentUser.id));
-  }, [currentUser]);
-
+const CartPageContainer = () => {
   return (
     <div className="cart-page">
       <ListCart />
