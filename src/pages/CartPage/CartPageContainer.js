07a8dc3d3e955c7852e0ef58f150008eff692df5
apache <nhaphan0073@gmail.com>
2020-08-14 18:03:25 +0700

    hangle change quantity

--- src/pages/CartPage/CartPageContainer.js
+++ src/pages/CartPage/CartPageContainer.js
@@ -1,26 +1,53 @@
-import React, { useEffect } from "react";
-import PropTypes from "prop-types";
-import ListCart from "./components/ListCart";
-import { useDispatch, useSelector } from "react-redux";
-import { fetchCart, deleteItem } from "../../redux/Cart/cart.actions";
+import React, { useEffect } from 'react';
+import PropTypes from 'prop-types';
+import { useDispatch, useSelector } from 'react-redux';
+import ListCart from './components/ListCart';
 
-const CartPageContainer = (props) => {
-  const currentUser = useSelector((state) => state.user.currentUser) || {};
+import {
+  fetchCart,
+  deleteItem,
+  decreaseQuantity,
+  increaseQuantity,
+  handleQuantity
+} from '../../redux/Cart/cart.actions';
+import CartTotal from './components/CartTotal';
 
-  const cart = useSelector((state) => state.cart) || {};
+const CartPageContainer = props => {
+  const currentUser = useSelector(state => state.user.currentUser) || {};
+
+  const cart = useSelector(state => state.cart) || {};
 
   const dispatch = useDispatch();
   useEffect(() => {
     if (currentUser.id !== undefined) dispatch(fetchCart(currentUser.id));
   }, [currentUser]);
 
-  const handleDelete = (id) => {
+  const handleDelete = id => {
     dispatch(deleteItem(id));
+  };
+
+  const handleDecrement = id => {
+    dispatch(decreaseQuantity(id));
+  };
+
+  const handleIncrement = id => {
+    dispatch(increaseQuantity(id));
+  };
+
+  const handleBlur = (id, pro_quantity) => {
+    dispatch(handleQuantity(id, Number(pro_quantity)));
   };
 
   return (
     <div className="cart-page">
-      <ListCart onDelete={handleDelete} cart={cart} />
+      <ListCart
+        onBlur={handleBlur}
+        onDecrement={handleDecrement}
+        onIncrement={handleIncrement}
+        onDelete={handleDelete}
+        cart={cart}
+      />
+      <CartTotal />
     </div>
   );
 };
--- src/pages/CartPage/components/ListCart/index.js
+++ src/pages/CartPage/components/ListCart/index.js
@@ -1,79 +1,108 @@
-import React from "react";
-import PropTypes from "prop-types";
-import { Table, Button, Input } from "antd";
-import { MinusOutlined, PlusOutlined } from "@ant-design/icons";
-import "./styles.scss";
-import { Link } from "react-router-dom";
-import { getPrice } from "../../../../utils/cart";
+import React, { useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
+import { Table, Button, Input } from 'antd';
+import { MinusOutlined, PlusOutlined } from '@ant-design/icons';
+import './styles.scss';
+import { Link } from 'react-router-dom';
+import { getPrice } from '../../../../utils/cart';
 
 const columns = [
   {
-    title: "Sản Phẩm",
-    dataIndex: "pro",
+    title: 'Sản Phẩm',
+    dataIndex: 'pro',
     width: 600,
-    render: (pro) => (
+    render: pro => (
       <div className="product-cart">
         <Link to="/">
           <img className="product-cart-img" src={pro.avatar} alt="" />
           <span>{pro.text}</span>
         </Link>
       </div>
-    ),
+    )
   },
   {
-    title: "Đơn Giá",
-    dataIndex: "price",
-    width: 150,
+    title: 'Đơn Giá',
+    dataIndex: 'price',
+    width: 150
   },
   {
-    title: "Số Lượng",
-    dataIndex: "quantity",
+    title: 'Số Lượng',
+    dataIndex: 'quantity',
     width: 150,
-    render: (quantity) => (
+    render: quantity => (
       <div className="detail-quantity">
         <div className="detail-number">
-          <Button icon={<MinusOutlined />} />
-          <Input defaultValue={quantity} readOnly={true} />
-          <Button icon={<PlusOutlined />} />
+          <Button onClick={quantity.onDecrement} icon={<MinusOutlined />} />
+          <Input
+            onBlur={quantity.onBlur}
+            onChange={quantity.onChange}
+            value={quantity.value}
+          />
+          <Button onClick={quantity.onIncrement} icon={<PlusOutlined />} />
         </div>
       </div>
-    ),
+    )
   },
   {
-    title: "Số Tiền",
-    dataIndex: "total",
-    width: 150,
+    title: 'Số Tiền',
+    dataIndex: 'total',
+    width: 150
   },
   {
-    title: "Thao Tác",
-    dataIndex: "delete",
+    title: 'Thao Tác',
+    dataIndex: 'delete',
     width: 150,
-    key: "x",
-    render: (onDelete) => (
+    key: 'x',
+    render: onDelete => (
       <div onClick={onDelete} className="product-cart-del">
         Xóa
       </div>
-    ),
-  },
+    )
+  }
 ];
 
-const ListCart = (props) => {
+const ListCart = props => {
   const {
     cart: { products = [] },
     onDelete,
+    onDecrement,
+    onIncrement,
+    onBlur
   } = props;
 
-  const newData = products.map((product) => {
+  const [productQuantity, setProductQuantity] = useState(products);
+  useEffect(() => {
+    const quantity = products.reduce(
+      (obj, product) => ({ ...obj, [product.pro_id]: product.pro_quantity }),
+      {}
+    );
+    setProductQuantity(quantity);
+  }, [props]);
+
+  const newData = products.map(product => {
     return {
       key: product.pro_id,
       pro: {
         text: product.pro_name,
-        avatar: product.pro_avatar,
+        avatar: product.pro_avatar
       },
       price: product.pro_price,
-      quantity: product.pro_quantity,
+      quantity: {
+        value: productQuantity[product.pro_id] || 0,
+        onDecrement: () => onDecrement(product.pro_id),
+        onIncrement: () => onIncrement(product.pro_id),
+        onChange: event => {
+          setProductQuantity({
+            ...productQuantity,
+            [product.pro_id]: event.target.value
+          });
+        },
+        onBlur: () => {
+          onBlur(product.pro_id, productQuantity[product.pro_id]);
+        }
+      },
       total: getPrice(product.pro_price, product.pro_quantity),
-      delete: () => onDelete(product.pro_id),
+      delete: () => onDelete(product.pro_id)
     };
   });
 
@@ -90,7 +119,7 @@ const ListCart = (props) => {
             columns={columns}
             dataSource={newData}
             rowSelection={{
-              type: "checkbox",
+              type: 'checkbox'
             }}
           />
         </div>
